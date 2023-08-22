1715f9410bdd8ffb4c578826d6a508266efa0cdf
Shaurya-Srivastav <ssrivastav@ucdavis.edu>
2023-08-22 15:12:11 -0700

    readme

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+# Import necessary libraries
 import csv
 import random
 from deap import base, creator, tools, algorithms
 
+# Function to read player data from a CSV file
 def read_data(filename):
     player_data = []
     with open(filename, 'r') as csvfile:
@@ -10,6 +12,7 @@ def read_data(filename):
             player_data.append(row)
     return player_data
 
+# Function to calculate the player score based on various factors
 def calculate_player_score(player):
     player_score = float(player['PlayerScore'])
     demand_score = float(player['DemandScore'])
@@ -17,11 +20,13 @@ def calculate_player_score(player):
     
     return player_score * demand_score * (1 / last_bid_price)
 
+# Function to evaluate the performance of a team
 def evaluate_team(individual, player_data, budget):
     total_price = 0
     total_score = 0
     category_counts = {'Batsmen': 0, 'All rounders': 0, 'Wicketkeeper': 0, 'Pacers': 0, 'Spinners': 0}
     
+    # Calculate total price, score, and count players in each category
     for i, selected in enumerate(individual):
         if selected:
             player = player_data[i]
@@ -29,6 +34,7 @@ def evaluate_team(individual, player_data, budget):
             total_score += calculate_player_score(player)
             category_counts[player['Category']] += 1
             
+    # Calculate penalties for exceeding budget and category count
     penalty = max(0, total_price - budget)
     
     category_penalty = 0
@@ -36,9 +42,12 @@ def evaluate_team(individual, player_data, budget):
         diff = abs(count - CATEGORY_REQUIREMENTS[category])
         category_penalty += diff
     
+    # Combine total score, penalty, and category penalty
     return total_score - penalty - category_penalty * 100,  # Adjusted penalty weight
 
+# Function to use a genetic algorithm to select players based on performance and budget
 def select_players_genetic(player_data, budget):
+    # Define the fitness function to be maximized (higher is better)
     creator.create("FitnessMax", base.Fitness, weights=(1.0,))
     creator.create("Individual", list, fitness=creator.FitnessMax)
     
@@ -50,39 +59,50 @@ def select_players_genetic(player_data, budget):
     toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n_players)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     
-    # Genetic operators
+    # Define genetic operators
     toolbox.register("mate", tools.cxTwoPoint)
     toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
     toolbox.register("select", tools.selTournament, tournsize=3)
     toolbox.register("evaluate", evaluate_team, player_data=player_data, budget=budget)
     
+    # Set genetic algorithm parameters
     population_size = 100
     generations = 50
     
+    # Initialize the population of individuals
     population = toolbox.population(n=population_size)
     
+    # Apply the genetic algorithm to evolve the population
     algorithms.eaSimple(population, toolbox, cxpb=0.7, mutpb=0.2, ngen=generations, verbose=False)
     
+    # Select the best individual from the evolved population
     best_individual = tools.selBest(population, k=1)[0]
     selected_players = [player_data[i] for i, selected in enumerate(best_individual) if selected]
     total_price_paid = sum(float(player['LastBidPrice']) for player in selected_players)
     
     return selected_players, total_price_paid
 
+# Define the required number of players in each category
 CATEGORY_REQUIREMENTS = {'Batsmen': 2, 'All rounders': 1, 'Wicketkeeper': 1, 'Pacers': 2, 'Spinners': 1}
 
+# Main function to run the player selection process
 def main():
     filename = 'Data.csv'
     budget = 35.0  # Set the budget to 35
     
+    # Read player data from the CSV file
     player_data = read_data(filename)
+    
+    # Select the best players using the genetic algorithm
     selected_players, total_price_paid = select_players_genetic(player_data, budget)
     
+    # Display the selected players and total price paid
     print("\nSelected Players:")
     for player in selected_players:
         print(f"{player['PlayerName']} - {player['Category']} (Demand: {player['DemandScore']}, Bid: ${float(player['LastBidPrice']):.2f})")
     
     print(f"\nTotal Price Paid for Selected Players: ${total_price_paid:.2f}")
 
+# Execute the main function if this script is run as the main module
 if __name__ == "__main__":
     main()
